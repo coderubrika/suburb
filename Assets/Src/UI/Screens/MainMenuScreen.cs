70147af364caffb4e721614afa95b05e24218e38
Anton Samartsev <coderubrika@gmail.com>
2023-05-10 01:05:45 +0300

    quit

--- Assets/Src/UI/Screens/MainMenuScreen.cs
+++ Assets/Src/UI/Screens/MainMenuScreen.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace Suburb.UI
 {
     public class MainMenuScreen : BaseScreen
     {
-        
+        [SerializeField] private Button newGameButton;
+        [SerializeField] private Button quitButton;
+
+        [Inject]
+        public void Construct()
+        {
+            quitButton.OnClickAsObservable()
+                .Subscribe(_ => Application.Quit())
+                .AddTo(this);
+        }
     }
 }
