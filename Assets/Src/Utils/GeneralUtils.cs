6435c7bda288bc3879337235e31fc23ec92e8d60
Anton Samartsev <coderubrika@gmail.com>
2023-06-24 13:33:17 +0300

    improved

---
+++ Assets/Src/Utils/GeneralUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UniRx;
+
+namespace Suburb.Utils
+{
+    public static class GeneralUtils
+    {
+        public static IObservable<T> StartWithDefault<T> (T defaultValue = default)
+        {
+            return Observable.Start(() => defaultValue);
+        }
+    }
+}
