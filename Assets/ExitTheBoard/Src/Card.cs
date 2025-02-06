e81547b8c33c004be7087bd61637b8bbf10fecf2
Anton Samartsev <coderubrika@gmail.com>
2025-02-06 15:50:56 +0300

    startup fit

--- Assets/ExitTheBoard/Src/Card.cs
+++ Assets/ExitTheBoard/Src/Card.cs
@@ -1,11 +1,101 @@
+using Suburb.Inputs;
+using Suburb.Utils;
+using UniRx;
 using UnityEngine;
+using Zenject;
 
 namespace ExitTheBoard
 {
     public class Card : MonoBehaviour, IMovable
     {
+        private LayerOrderer layerOrderer;
+        private GORectSession inputSession;
+        private MouseSwipeCompositor mouseSwipeCompositor;
+        private SwipeMember swipe;
+        private Camera viewCamera;
+        
         [SerializeField] private BoxCollider boxCollider;
+
+        private readonly CompositeDisposable disposables = new();
+        
+        private Vector2 screenPosition;
+        private LineTrack track;
+        
         public Transform Transform => transform;
         public BoxCollider BoxCollider => boxCollider;
+        
+        [Inject]
+        private void Construct(
+            LayerOrderer layerOrderer,
+            InjectCreator injectCreator)
+        {
+            this.layerOrderer = layerOrderer;
+            viewCamera = Camera.main;
+            
+            //tmp for test
+            track = new LineTrack(new Vector3(4.26f, 0, -9.11f), new Vector3(-4.21f, 0, -9.11f));
+            
+            inputSession = injectCreator.Create<GORectSession>(new GORectSessionParams
+            {
+                Camera = Camera.main,
+                Target = gameObject
+            });
+            
+            mouseSwipeCompositor = injectCreator.Create<MouseSwipeCompositor>(MouseButtonType.Left);
+            swipe = inputSession.GetMember<SwipeMember>();
+            inputSession.SetBookResources(true);
+        }
+        
+        public void Activate()
+        {
+            inputSession.AddCompositor(mouseSwipeCompositor)
+                .AddTo(disposables);
+            layerOrderer.ConnectFirst(inputSession)
+                .AddTo(disposables);
+            
+            SetupInputHandle();
+        }
+
+        public void Deactivate()
+        {
+            disposables.Clear();
+        }
+
+        private void SetupInputHandle()
+        {
+            swipe.OnDown
+                .Subscribe(pos => screenPosition = pos)
+                .AddTo(disposables);
+            
+            swipe.OnDrag
+                .Subscribe(screenDelta =>
+                {
+                    Vector2 newScreenPosition = screenPosition + screenDelta;
+                    
+                    Vector3 cardPositionStart = viewCamera.ScreenToWorldPoint(screenPosition);
+                    Vector3 cardPositionEnd = viewCamera.ScreenToWorldPoint(newScreenPosition);
+                    
+                    Vector3 delta = cardPositionEnd - cardPositionStart;
+                    Vector3 dirOne = track.DirectionOne;
+                    float deltaProj = Vector3.Dot(dirOne, delta);
+                    Vector3 pos = Transform.position;
+
+                    Vector3 dir = deltaProj > 0 ? track.Direction : -track.Direction;
+                    int sign = Vector3.Dot(dir, Transform.forward) > 0 ? 1 : -1;
+                    Vector3 side = Transform.forward * BoxCollider.size.z * 0.5f * Transform.localScale.z * sign;
+                    
+                    float trackLen = track.Length;
+                    Vector3 objDir = pos + side - track.StartPoint;
+                    float objProj = Vector3.Dot(objDir, dirOne);
+                    Vector3 objParallel = dirOne * objProj;
+                    Vector3 objPerp = objDir - objParallel;
+                    float deltaByObjProj = deltaProj + objProj;
+                    float clampedProj = Mathf.Clamp(deltaByObjProj, 0, trackLen);
+                    Vector3 newObjParallel = dirOne * clampedProj;
+                    Vector3 finalPos = track.StartPoint + newObjParallel + objPerp;
+                    Transform.position = finalPos - side;
+                })
+                .AddTo(disposables);
+        }
     }
 }
--- Assets/ExitTheBoard/Src/Startup.cs
+++ Assets/ExitTheBoard/Src/Startup.cs
@@ -9,107 +9,33 @@ namespace ExitTheBoard
 {
     public class Startup : MonoBehaviour
     {
-        private LayerOrderer layerOrderer;
-        private InjectCreator injectCreator;
-        private GORectSession cardSession;
-        
-        [SerializeField] private RectTransform frame;
-        [SerializeField] private Camera mainCamera;
         [SerializeField] private PointsAnchorMono pointsAnchor;
 
         [SerializeField] private Card card;
-        [SerializeField] private Rotator rotator;
+        [SerializeField] private Card rotator;
         
-        private readonly Dictionary<GameObject, object> interactables = new();
         private readonly CompositeDisposable disposables = new();
         private PointNode pointNode;
         private int endIndex;
         private LineTrack track;
         
-        private bool isMoved;
-        private Vector2 screenPosition;
-        private Vector3 offset;
-        private IMovable movable;
-        
         [Inject]
-        private void Construct(
-            LayerOrderer layerOrderer, 
-            InjectCreator injectCreator)
+        private void Construct()
         {
-            this.layerOrderer = layerOrderer;
-            this.injectCreator = injectCreator;
-            interactables.Add(card.gameObject, card);
-            interactables.Add(rotator.gameObject, rotator);
-            cardSession = injectCreator.Create<GORectSession>(new GORectSessionParams
-            {
-                Camera = mainCamera,
-                Target = card.gameObject
-            });
             (pointNode, endIndex) = pointsAnchor.GetStartEndPoints();
             track = new LineTrack(pointNode.Position, pointNode.NeighboursPoints[endIndex].Position);
         }
-
+        
         private void OnEnable()
         {
-            MouseSwipeCompositor compositor = injectCreator.Create<MouseSwipeCompositor>(MouseButtonType.Left);
-            cardSession.AddCompositor(compositor)
-                .AddTo(disposables);
-            layerOrderer.ConnectFirst(cardSession)
-                .AddTo(disposables);
-            
-            SwipeMember swipe = cardSession.GetMember<SwipeMember>();
-            swipe.OnDown
-                .Subscribe(pos =>
-                {
-                    movable = card;
-                    isMoved = movable != null;
-                    screenPosition = pos;
-                })
-                .AddTo(disposables);
-            
-            swipe.OnDrag
-                .Subscribe(screenDelta =>
-                {
-                    if (!isMoved)
-                        return;
-                    Vector2 newScreenPosition = screenPosition + screenDelta;
-                    
-                    Vector3 cardPositionStart = mainCamera.ScreenToWorldPoint(screenPosition);
-                    Vector3 cardPositionEnd = mainCamera.ScreenToWorldPoint(newScreenPosition);
-                    
-                    Vector3 delta = cardPositionEnd - cardPositionStart;
-                    Vector3 dirOne = track.DirectionOne;
-                    float deltaProj = Vector3.Dot(dirOne, delta);
-                    Vector3 pos = movable.Transform.position;
-
-                    Vector3 dir = deltaProj > 0 ? track.Direction : -track.Direction;
-                    int sign = Vector3.Dot(dir, movable.Transform.forward) > 0 ? 1 : -1;
-                    Vector3 side = movable.Transform.forward * movable.BoxCollider.size.z * 0.5f * movable.Transform.localScale.z * sign;
-                    
-                    float trackLen = track.Length;
-                    Vector3 objDir = pos + side - track.StartPoint;
-                    float objProj = Vector3.Dot(objDir, dirOne);
-                    Vector3 objParallel = dirOne * objProj;
-                    Vector3 objPerp = objDir - objParallel;
-                    float deltaByObjProj = deltaProj + objProj;
-                    float clampedProj = Mathf.Clamp(deltaByObjProj, 0, trackLen);
-                    Vector3 newObjParallel = dirOne * clampedProj;
-                    Vector3 finalPos = track.StartPoint + newObjParallel + objPerp;
-                    movable.Transform.position = finalPos - side;
-                })
-                .AddTo(disposables);
-            
-            swipe.OnUp
-                .Subscribe(pos =>
-                {
-                    movable = null;
-                    isMoved = false;
-                })
-                .AddTo(disposables);
+            card.Activate();
+            rotator.Activate();
         }
 
         private void OnDisable()
         {
+            card.Deactivate();
+            rotator.Deactivate();
             disposables.Clear();
         }
     }
